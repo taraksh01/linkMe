10beee3d101f2cd4c67b44f2293aa22d78ceafae
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-11-19 09:32:01 +0530

    Created Logoutbutton with logout functionality

---
+++ src/components/LogoutButton.js
@@ -0,0 +1,22 @@
+import { useDispatch } from "react-redux";
+import authService from "../appwrite/authConfig";
+import { logout as sliceLogout } from "../store/features/authSlice";
+import Button from "./Button";
+import { useNavigate } from "react-router-dom";
+
+const LogoutButton = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const logout = () => {
+    authService.logout().then((res) => {
+      if (res === "Logged out successfully") {
+        dispatch(sliceLogout());
+        navigate("/");
+      }
+    });
+  };
+
+  return <Button onClick={logout}>Log out</Button>;
+};
+
+export default LogoutButton;
