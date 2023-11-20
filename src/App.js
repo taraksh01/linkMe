a0641cbe250c2af07dac4b4e95b86b62b65a9ed0
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-11-20 19:40:23 +0530

    Rendering App

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+import { Outlet } from "react-router-dom";
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Outlet />
+    </div>
+  );
+};
+
+export default App;
