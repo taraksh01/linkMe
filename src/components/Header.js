59655de8e70903318c4e6aedc6581d0ff403b3fc
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-11-20 19:39:35 +0530

    Created Header of the app

---
+++ src/components/Header.js
@@ -0,0 +1,42 @@
+import { useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import Button from "./Button";
+import Logo from "./Logo";
+import LogoutButton from "./LogoutButton";
+
+const Header = () => {
+  const authStatus = useSelector((state) => state.authSlice.status);
+  const navigate = useNavigate();
+  const navItems = [
+    { name: "Home", url: "/", active: true },
+    { name: "Login", url: "/login", active: authStatus !== "authorized" },
+    {
+      name: "Create Account",
+      url: "/register",
+      active: authStatus !== "authorized",
+    },
+  ];
+
+  return (
+    <header className="flex justify-between items-center shadow-md p-2 w-screen">
+      <Link to={"/"}>
+        <Logo>LinkMe</Logo>
+      </Link>
+      <div className="flex gap-4">
+        <div className="">
+          {navItems?.map(
+            (item) =>
+              item.active && (
+                <Button key={item.name} onClick={() => navigate(item.url)}>
+                  {item.name}
+                </Button>
+              )
+          )}
+        </div>
+        {authStatus === "authorized" && <LogoutButton />}
+      </div>
+    </header>
+  );
+};
+
+export default Header;
