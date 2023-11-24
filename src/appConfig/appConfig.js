3ec0c0c53f7d31884de4367259e6adf51cbfd403
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-11-24 15:25:19 +0530

    Added post related data

--- src/appConfig/appConfig.js
+++ src/appConfig/appConfig.js
@@ -3,7 +3,9 @@ const appConfig = {
   appwriteProjectId: String(process.env.APPWRITE_PROJECT_ID),
   appwriteUserDbId: String(process.env.APPWRITE_USER_DB_ID),
   appwriteUserCollectionId: String(process.env.APPWRITE_USER_COLLECTION_ID),
+  appwritePostDbId: String(process.env.APPWRITE_POST_DB_ID),
+  appwritePostCollectionId: String(process.env.APPWRITE_POST_COLLECTION_ID),
+  appwritePostDataId: String(process.env.APPWRITE_POST_DATA_ID),
 };
-
 
 export default appConfig;
