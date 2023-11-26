96a738a068ae770ff9ebc5a5c75a412435543c4a
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-11-26 19:39:17 +0530

    Showing all the posts on homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,25 @@
 import { useSelector } from "react-redux";
+import Post from "./Post";
+import databaseService from "../appwrite/postConfig";
+import { useEffect } from "react";
+import { useState } from "react";
 
 const Home = () => {
   const authStatus = useSelector((state) => state.authSlice.status);
+  const [allPosts, setAllPosts] = useState([]);
+
+  useEffect(() => {
+    databaseService.getAllPosts().then((res) => setAllPosts(res));
+  }, []);
   return authStatus === "authorized" ? (
-    <h1 className="text-center text-5xl m-2">Create a post</h1>
+    <>
+      <h1 className="text-center text-5xl m-2">Create a new post</h1>
+      <div className="flex flex-wrap mx-auto max-w-sm bg-zinc-950 justify-center">
+        {allPosts?.documents?.map((post) => (
+          <Post key={post.$id}>{post}</Post>
+        ))}
+      </div>
+    </>
   ) : (
     <h1 className="text-center text-5xl m-2">Login to create posts</h1>
   );
