c799f888a587cc29a9e49d413199bf3054e8fb6e
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-11-27 18:35:42 +0530

    If the user data is present in local storage, dispatching login function

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import { useEffect } from "react";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import { Outlet } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { login as sliceLogin } from "./store/features/authSlice";
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const loggedInUser = localStorage.getItem("loggedInUser");
+    if (loggedInUser) {
+      dispatch(sliceLogin(loggedInUser));
+    }
+  }, []);
+
   return (
     <div>
       <Header />
