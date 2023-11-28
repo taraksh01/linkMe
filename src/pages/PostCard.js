4a0ad3d3b478533a267785cb7f9215fd4b1dd86e
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-11-28 10:18:59 +0530

    Changed bg color and added margin to post

--- src/pages/PostCard.js
+++ src/pages/PostCard.js
@@ -10,7 +10,7 @@ const Post = (posts) => {
     userId,
   } = posts.children;
   return (
-    <div className="m-2 rounded-lg bg-gray-300 p-2 w-80 mx-auto">
+    <div className="m-1 rounded-lg bg-gray-100 p-2 w-full mx-auto">
       <div className="flex">
         <div className="rounded-full w-12 h-12 bg-black"></div>
         <div className="flex flex-col ml-2 text-sm justify-center">
@@ -19,7 +19,7 @@ const Post = (posts) => {
         </div>
         <div className="px-2 self-start ml-auto text-2xl">⋮</div>
       </div>
-      <div className="text-2xl text-justify">{post}</div>
+      <div className="text-2xl text-justify my-1">{post}</div>
     </div>
   );
 };
