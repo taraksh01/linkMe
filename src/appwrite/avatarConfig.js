38ee082547df92ecf5f36369b946f456f8d03f16
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-11-29 14:56:10 +0530

    Created methods to get image url of user's initial letter, qr ,favicon, flag

--- src/appwrite/avatarConfig.js
+++ src/appwrite/avatarConfig.js
@@ -12,6 +12,26 @@ class AvatarService {
 
     this.avatar = new Avatars(this.client);
   }
+
+  getUserInitial(name, width = 50, height = 50) {
+    return this.avatar.getInitials(name, width, height);
+  }
+
+  getQRCode(text, size = 400) {
+    return this.avatar.getQR(text, size);
+  }
+
+  getImage(url, width = 400, height = 400) {
+    return this.avatar.getImage(url, width, height);
+  }
+
+  getCountryFlag(countryCode, width = 50, height = 50) {
+    return this.avatar.getFlag(countryCode, width, height);
+  }
+
+  getFavicon(url) {
+    return this.avatar.getFavicon(url);
+  }
 }
 
 const avatarService = new AvatarService();
