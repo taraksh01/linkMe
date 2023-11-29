cf3ee42b2bf31a0b593821907da8a5cad23652e2
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-11-29 16:11:48 +0530

    Created method to created file in database

--- src/appwrite/userStorage.js
+++ src/appwrite/userStorage.js
@@ -12,6 +12,21 @@ class UserStorage {
 
     this.storage = new Storage(this.client);
   }
+
+  async createFile(file) {
+    try {
+      const response = await this.storage.createFile(
+        appConfig.appwriteUserImage,
+        ID.unique(),
+        file
+      );
+      if (response) {
+        return response;
+      }
+    } catch (error) {
+      return error.message;
+    }
+  }
 }
 
 const userStorage = new UserStorage();
