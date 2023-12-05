c6ab28189d40a1c8de16362fa0501c1ddd3b78ae
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-12-05 11:01:34 +0530

    Created profile

---
+++ src/pages/Profile.js
@@ -0,0 +1,40 @@
+import { useSelector } from "react-redux";
+import userService from "../appwrite/userConfig";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+const Profile = () => {
+  const [userDetails, setUserDetails] = useState({
+    fullName: "Full Name",
+    userName: "username",
+  });
+  const user = useSelector((state) => state?.authSlice?.user);
+  useEffect(() => {
+    fetchUserDetails();
+  }, []);
+
+  const fetchUserDetails = async () => {
+    setUserDetails(await userService.getUser({ userId: user?.$id }));
+  };
+
+  return (
+    <Link
+      className="flex items-center gap-2 px-1 rounded-lg sm:bg-slate-200"
+      to={`user/${userDetails.userName}`}
+    >
+      <div className="flex flex-col justify-evenly max-sm:hidden">
+        <h1 className="text-lg font-medium capitalize">
+          {userDetails?.fullName}
+        </h1>
+        <h2 className="lowercase">{userDetails.userName}</h2>
+      </div>
+      <img
+        src={userDetails?.userImage}
+        alt={userDetails?.userName}
+        className="bg-black rounded-full border border-black w-12 h-12"
+      />
+    </Link>
+  );
+};
+
+export default Profile;
