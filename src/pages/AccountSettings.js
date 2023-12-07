d8ef4431e9c444273a75b8cf33af1c95cc15365a
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-12-07 13:16:19 +0530

    Moved updation code to separate file

--- src/pages/AccountSettings.js
+++ src/pages/AccountSettings.js
@@ -1,35 +1,21 @@
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import userService from "../appwrite/userConfig";
-import { useForm } from "react-hook-form";
-import Input from "../components/Input";
 import Button from "../components/Button";
 import { Link } from "react-router-dom";
+import UpdateUserDetails from "./UpdateUserDetails";
+import UpdateProfilePic from "./UpdateProfilePic";
 
 const AccountSettings = () => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
   const user = useSelector((state) => state?.authSlice?.user);
   const [userDetails, setUserDetails] = useState({});
-  const [message, setMessage] = useState(null);
   const [tab, setTab] = useState("Account");
 
   useEffect(() => {
     userService.getUser({ userId: user?.$id }).then((res) => {
       setUserDetails(res);
     });
-  }, [userDetails]);
-
-  const updateDetails = async (data) => {
-    const updatedDetails = await userService.updateUser({
-      userId: user?.$id,
-      ...data,
-    });
-    setUserDetails(updatedDetails);
-  };
+  }, [user]);
 
   return (
     <div className="max-w-2xl mx-auto">
@@ -75,60 +61,17 @@ const AccountSettings = () => {
         <h1>Account</h1>
       </div>
       <div className={`flex ${tab === "User Details" ? "flex" : "hidden"}`}>
-        <form
-          onSubmit={handleSubmit(updateDetails)}
-          className="flex flex-col justify-center w-full items-center mx-auto"
-        >
-          <Input
-            label="Full Name"
-            type="text"
-            placeholder="Enter your full name"
-            required={true}
-            error={errors?.fullName}
-            defaultValue={userDetails?.fullName}
-            {...register("fullName", { required: true })}
-          />
-          <Input
-            label="Username"
-            type="text"
-            placeholder="Choose a username"
-            required={true}
-            error={errors?.userName}
-            defaultValue={userDetails?.userName}
-            message={message}
-            {...register("userName", {
-              required: true,
-              onChange: async (e) => {
-                setMessage(
-                  e.target.value === ""
-                    ? "username can not be empty"
-                    : await userService.isAvailable("userName", e.target.value)
-                );
-              },
-            })}
-          />
-          <Input
-            label="Email"
-            type="email"
-            placeholder="Enter your email"
-            required={true}
-            error={errors?.email}
-            defaultValue={userDetails?.email}
-            {...register("email", { required: true })}
-          />
-          <Button type="submit" className="m-4 shadow-md">
-            Update Details
-          </Button>
-        </form>
+        <UpdateUserDetails
+          userDetails={userDetails}
+          setUserDetails={setUserDetails}
+          userId={user?.$id}
+        />
       </div>
       <div className={`${tab === "Profile pic" ? "flex" : "hidden"}`}>
-        <Input
-          label="Profile Pic"
-          type="file"
-          required={true}
-          error={errors?.fullName}
-          defaultValue={userDetails?.profilePic}
-          {...register("profilePic", { required: true })}
+        <UpdateProfilePic
+          userDetails={userDetails}
+          setUserDetails={setUserDetails}
+          userId={user?.$id}
         />
       </div>
     </div>
