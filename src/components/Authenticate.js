dd0af3ebabb7f152b5757bdb830ce5f41494c217
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-12-08 10:20:14 +0530

    Created component that authenticate the user and then let's the user use the subDomain

---
+++ src/components/Authenticate.js
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+const Authenticate = ({ children }) => {
+  const url = window.location.href;
+  const { user } = useSelector((state) => state.authSlice);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    !user && (url.includes("login") || url.includes("register"))
+      ? ""
+      : navigate("/login");
+    user && (url.includes("login") || url.includes("register"))
+      ? navigate("/")
+      : "";
+  }, [user, url]);
+
+  return <>{children}</>;
+};
+
+export default Authenticate;
